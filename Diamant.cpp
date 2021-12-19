98ae77ca61649c460f262b5586093036a5f6fef4
adame <adame@DESKTOP-VOPV20U>
2021-12-19 18:04:06 +0100

    Přidejte soubory projektu.

---
+++ Diamant.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int mezera = 50;
+
+	//Pyramida klasicka
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < mezera; j++) {
+			printf(" ");
+
+		}
+
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+
+		printf("\n");
+		mezera--;
+
+	}
+	//Pyramida obracena
+	mezera = 41;
+
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < mezera; j++) {
+			printf(" ");
+
+		}
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+		printf("\n");
+		mezera++;
+
+	}
+
+}
